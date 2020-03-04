a8236815eb2de6b9bc584fb1c0828948db68b451
Paul Nicholsen <paulnicholsen27@gmail.com>
2020-03-04 16:44:55 -0500

    Update seeds.rb
    typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,6 @@ Planet.destroy_all
 15.times {Scientist.create(name: Faker::FunnyName.name,
                            field_of_study: Faker::Educator.subject)}
 
-20.times {Mission.create(name: Faker::TvShows::Buffy.unqiue.episode,
+20.times {Mission.create(name: Faker::TvShows::Buffy.unique.episode,
                          scientist: Scientist.all.sample, 
                          planet: Planet.all.sample)}
